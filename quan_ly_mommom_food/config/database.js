058c7cccc9d18b356c9abfce0be1a559644dec27
tranbinh1408 <tranthibinh6626@gmail.com>
2024-12-24 10:38:18 +0700

    vhj

--- quan_ly_mommom_food/config/database.js
+++ quan_ly_mommom_food/config/database.js
@@ -1,21 +1,11 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  port: 3308,
-  user: 'mommom_food',
-  password: '1',
-  database: 'mommom_food'
+  host: 'mysql.railway.internal',
+  port: 3306,
+  user: 'root',
+  password: 'oWNPK1SixOPZyqDKNM1OrYXbFYWsGRXb',
+  database: 'railway'
 });
 
-// Test connection
-db.getConnection()
-  .then(connection => {
-    console.log('Database connected successfully');
-    connection.release();
-  })
-  .catch(err => {
-    console.error('Error connecting to the database:', err);
-  });
-
-module.exports = db;
+module.exports = db;   
